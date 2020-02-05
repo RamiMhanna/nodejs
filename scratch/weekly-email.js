2db69ef0a95065f0c672576f302631b9bab95464
Rami Mhanna <rami.mhanna@powercoders.org>
2020-02-05 09:21:07 +0100

    Using events module

---
+++ scratch/weekly-email.js
@@ -0,0 +1,8 @@
+const Job = require('./job.js')
+let job = new Job()
+
+job.on('done', function(details){
+    console.log('Weekly email job was completed at', details.completedOn)
+})
+
+job.emit('start')
