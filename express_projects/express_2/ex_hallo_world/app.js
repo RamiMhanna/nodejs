0032f6b22b7acf66439e65c02b3452681b8aa983
Rami Mhanna <rami.mhanna@powercoders.org>
2020-02-09 23:52:38 +0100

    Add configration to the Express app like the port and the place where the template will be stored

--- express_projects/express_2/ex_hallo_world/app.js
+++ express_projects/express_2/ex_hallo_world/app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+app.set('port', process.env.PORT || 3000)
+app.set('views', 'templates')
+app.set('view engine', 'jade')
 app.get('/', (req,res) => {
     res.send('Hello World')
 })
