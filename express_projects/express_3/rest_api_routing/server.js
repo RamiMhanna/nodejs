f545831461681122b316e15317a2d216a0af71b0
Rami Mhanna <rami.mhanna@powercoders.org>
2020-02-14 00:22:42 +0100

    Accessing url parameters and use CRUD restful api server and Validate the incoming data

--- express_projects/express_3/rest_api_routing/server.js
+++ express_projects/express_3/rest_api_routing/server.js
@@ -4,30 +4,38 @@ const bodyParser = require('body-parser')
 
 app.use(bodyParser.json())
 
-let profile = {
-    username : 'Rami',
-    email : '[reducted]',
-    url : 'http://ramimhanna.com'
-}
+let profile = [{
+    first_name:"Rami", 
+    last_name:"Mhanna"
+}]
 
 app.get('/profile', (req,res) => {
+    if (req.query.id) return res.send(profile[req.query.id])
     res.send(profile)
 })
 
 app.post('/profile', (req, res) => {
-    profile = req.body
+    if (!req.body.first_name.trim() || !req.body.last_name.trim()) {
+        //Client Error
+        return res.sendStatus(400)
+    }
+    let obj = {
+        first_name : req.body.first_name,
+        last_name : req.body.last_name
+    }
+    profile.push(obj)
     console.log('created', profile)
     res.sendStatus(201)
 })
 
-app.put('/profile', (req, res) => {
-    Object.assign(profile, req.body)
-    console.log('updated', profile)
+app.put('/profile/:id', (req, res) => {
+    Object.assign(profile[req.params.id], req.body)
+    console.log('updated', profile[req.params.id])
     res.sendStatus(204)
 })
 
-app.delete('/profile', (req, res) => {
-    profile = {}
+app.delete('/profile/:id', (req, res) => {
+    profile.splice(req.params.id, 1)
     console.log('deleted', profile)
     res.sendStatus(204)
 })
--- express_projects/express_3/rest_api_routing/tst.sh
+++ express_projects/express_3/rest_api_routing/tst.sh
@@ -1,8 +1,11 @@
-curl "http://localhost:3000/profile"
+curl "http://localhost:3000/profile/"
+sleep 1
 curl -H "Content-Type: application/json" -X POST -d '{"first_name":"Amin","last_name":"Mhanna"}' "http://localhost:3000/profile"
 sleep 1
-curl -H "Content-Type: application/json" -X PUT -d '{"first_name":"Yamen","last_name":"Mhanna"}' "http://localhost:3000/profile"
+curl -H "Content-Type: application/json" -X PUT -d '{"first_name":"Yamen","last_name":"Mhanna"}' "http://localhost:3000/profile/0"
 sleep 1
 curl "http://localhost:3000/profile"
 sleep 1
-curl -X DELETE "http://localhost:3000/profile"
+curl "http://localhost:3000/profile?id=0"
+sleep 1
+curl -X DELETE "http://localhost:3000/profile/0"
