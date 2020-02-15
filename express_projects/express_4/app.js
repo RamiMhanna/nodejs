3c5617362569f21866b1d10be52fee7139fad666
Rami Mhanna <rami.mhanna@powercoders.org>
2020-02-15 19:38:04 +0100

    Change the hard coded PORT to be dynamiclly  selected from the process environment variables

--- express_projects/express_4/app.js
+++ express_projects/express_4/app.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
     res.status(200).send('Hello')
 })
 
+app.get('/api/costumers', (req, res) => {
+    res.status(200).send([1, 2, 3])
+})
 
-app.listen(5000, () => {
-    console.log('Listening on port 5000')
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
 })
